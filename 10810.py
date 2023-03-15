d3d5926672cfa989f92d83804f6120d6838d5830
yhsphd <yhsphd05@gmail.com>
2023-03-15 20:49:31 +0900

    20230310

---
+++ 10810.py
@@ -0,0 +1,10 @@
+mn = list(map(int, input().split()))
+
+baskets = [0] * mn[0]
+
+for i in range(mn[1]):
+    ijk = list(map(int, input().split()))
+    for j in range(ijk[0]-1, ijk[1]):
+        baskets[j] = ijk[2]
+
+print(" ".join(map(str, baskets)))
