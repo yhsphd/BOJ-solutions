12dac2ddab147cc6b06f7f8f5640dcba85c41911
yhsphd <yhsphd05@gmail.com>
2023-03-17 16:49:07 +0900

    20230317

---
+++ 1018.py
@@ -0,0 +1,11 @@
+# WIP
+row = ["WB"*4, "BW"*4]
+chess = [[row[0], row[1]]*4, [row[1], row[0]]*4]
+
+size = int(input().split()[0])
+board = []
+
+for i in range(size):
+    board.append(input())
+
+# for i in range(len(board[0])-len(chess[0])):
---
+++ 1259.py
@@ -0,0 +1,14 @@
+while True:
+    flag = True
+    num = input()
+    if num == "0":
+        break
+
+    for i in range(len(num)//2):
+        if num[i] != num[-1-i]:
+            print("no")
+            flag = False
+            break
+
+    if flag:
+        print("yes")
